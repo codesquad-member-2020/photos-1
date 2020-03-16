264fb3f50f398547152bf85b55a513f350b55dad
TTOzzi <devttozzi@gmail.com>
2020-03-16 15:51:50 +0900

    photoCollectionView 의 dataSource 지정
    photoCollectionView 의 dataSource 를 만들어둔 PhotoCollectionViewDataSource 로 설정해줌

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    private let photoDataSource = PhotoCollectionViewDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        photoCollectionView.dataSource = photoDataSource
     }
-
-
 }
 
