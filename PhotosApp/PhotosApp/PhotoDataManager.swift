727e6dd152853b588cb3c7f0c434b1b6116cf862
TTOzzi <devttozzi@gmail.com>
2020-03-17 15:48:08 +0900

    PhotoDataManager 클래스 생성

---
+++ PhotosApp/PhotosApp/PhotoDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoDataManager.swift
+//  PhotosApp
+//
+//  Created by TTOzzi on 2020/03/17.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoDataManager {
+    
+}
