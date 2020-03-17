b3219ca270b3b32d25a95c8f88643b37a8d4945e
TTOzzi <devttozzi@gmail.com>
2020-03-17 11:57:31 +0900

    코드로 설정해줬던 Cell 의 커스텀클래스를 스토리보드로 설정하도록 수정

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         photoCollectionView.dataSource = photoDataSource
-        photoCollectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.reuseIdentifier)
     }
 }
 
