4d31d13b8ffba969ba1f8071198b7ad3ebf9dcac
TTOzzi <devttozzi@gmail.com>
2020-03-17 22:06:48 +0900

    PhotoLibrary 에 변동사항 발생 시 콜렉션뷰 로드 로직 변경
    콜렉션 뷰 전체를 변경하는게 아니라 변경된 부분만 로드하도록 변경

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -20,12 +20,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         photoCollectionView.dataSource = photoDataSource
-        NotificationCenter.default.addObserver(self, selector: #selector(reloadCollectionView), name: NSNotification.Name(rawValue: PhotoDataManager.photoLibraryChanged), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(photoRemoveUpdate), name: NSNotification.Name(rawValue: PhotoDataManager.photoRemoved), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(photoInsertUpdate), name: NSNotification.Name(rawValue: PhotoDataManager.photoInserted), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(photoChangeUpdate), name: NSNotification.Name(rawValue: PhotoDataManager.photoChanged), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(photoEnumerateUpdate), name: NSNotification.Name(rawValue: PhotoDataManager.photoEnumerated), object: nil)
     }
     
-    @objc func reloadCollectionView() {
-        DispatchQueue.main.async {
-            self.photoCollectionView.reloadData()
+    @objc func photoRemoveUpdate(_ notification: Notification) {
+        DispatchQueue.main.sync {
+            guard let receivedData = notification.userInfo else { return }
+            photoCollectionView.performBatchUpdates({
+                guard let index = receivedData[PhotoDataManager.photoRemoved] as? IndexSet else { return }
+                self.photoCollectionView.deleteItems(at: index.map { IndexPath(item: $0, section: 0) })
+            }, completion: nil)
+        }
+    }
+    
+    @objc func photoInsertUpdate(_ notification: Notification) {
+        DispatchQueue.main.sync {
+            guard let receivedData = notification.userInfo else { return }
+            photoCollectionView.performBatchUpdates({
+                guard let index = receivedData[PhotoDataManager.photoInserted] as? IndexSet else { return }
+                self.photoCollectionView.insertItems(at: index.map { IndexPath(item: $0, section: 0) })
+            }, completion: nil)
+        }
+    }
+    
+    @objc func photoChangeUpdate(_ notification: Notification) {
+        DispatchQueue.main.sync {
+            guard let receivedData = notification.userInfo else { return }
+            photoCollectionView.performBatchUpdates({
+                guard let index = receivedData[PhotoDataManager.photoChanged] as? IndexSet else { return }
+                self.photoCollectionView.reloadItems(at: index.map { IndexPath(item: $0, section: 0) })
+            }, completion: nil)
+        }
+    }
+    
+    @objc func photoEnumerateUpdate(_ notification: Notification) {
+        DispatchQueue.main.sync {
+            guard let receivedData = notification.userInfo else { return }
+            photoCollectionView.performBatchUpdates({
+                guard let index = receivedData[PhotoDataManager.photoEnumerated] as? (Int, Int) else { return }
+                self.photoCollectionView.moveItem(at: IndexPath(item: index.0, section: 0), to: IndexPath(item: index.1, section: 0))
+            }, completion: nil)
         }
     }
 }
