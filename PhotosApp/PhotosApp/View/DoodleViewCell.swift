6f61a6feb92bef8a52b5e197e621dd08ab40d8b1
TTOzzi <devttozzi@gmail.com>
2020-03-18 17:26:03 +0900

    DoodleViewCell 에 ImageView 추가
    이미지뷰가 셀에 꽉 차게 출력되도록 오토레이아웃 설정

--- PhotosApp/PhotosApp/View/DoodleViewCell.swift
+++ PhotosApp/PhotosApp/View/DoodleViewCell.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class DoodleViewCell: UICollectionViewCell {
     static let reuseIdentifier = "doodleCell"
+    let doodleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpUI()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        setUpUI()
+    }
+    
+    func setUpUI() {
+        addSubview(doodleImageView)
+        doodleImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        doodleImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        doodleImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        doodleImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     }
 }
