265735295a86553946b3785fea10dba79caeec70
TTOzzi <devttozzi@gmail.com>
2020-03-18 14:23:57 +0900

    DoodleViewController 생성
    UICollectionViweController 를 상속받음

---
+++ PhotosApp/PhotosApp/DoodleViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DoodleViewController.swift
+//  PhotosApp
+//
+//  Created by TTOzzi on 2020/03/18.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class DoodleViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
