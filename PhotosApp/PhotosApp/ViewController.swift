9adf4568fef6af757cfae96fcdd46d0a9be58f73
TTOzzi <devttozzi@gmail.com>
2020-03-18 16:23:33 +0900

    add 버튼 클릭 시 DoodleViewController present 구현

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var photoCollectionView: UICollectionView!
     
+    private let app = UIApplication.shared.delegate as! AppDelegate
     private let photoDataSource = PhotoCollectionViewDataSource()
     
     deinit {
@@ -21,6 +22,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         photoCollectionView.dataSource = photoDataSource
         addPhotoObservers()
+    }
+    
+    @IBAction func addButtonTabbed(_ sender: Any) {
+        self.present(app.doodleNavigationController, animated: true, completion: nil)
     }
 }
 
